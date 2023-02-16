2e2ff29276ecadfc8a9099539a6cd55fe032079a
so1454 <122778253+so1454@users.noreply.github.com>
2023-02-16 20:56:38 +0900

    0216

---
+++ calculator.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#이 기능을 가진 모듈을 만들것임
+
+def add(num1,num2) :
+    return num1 +num2
+
+def minus(num1,num2) :
+    return num1 -num2
+
+def mul(num1,num2) :
+    return num1 *num2
+
+def div(num1,num2) :
+    return num1 /num2
+
+
+# In[ ]:
+
+
+# 내 pc - c드라이브 - 사용자 - smhrd - Python Library로 들어가서 
+# 내가 python으로 빼서 만들어둔 calculator 옮기기
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
